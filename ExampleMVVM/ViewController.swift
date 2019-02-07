8382d09d57f78430c230de5b4f984f5cde805c7d
PabloRosalvo <pablo.rosalvo2014.2@gmail.com>
2019-02-07 14:20:03 -0200

    Initial Commit

---
+++ ExampleMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleMVVM
+//
+//  Created by Pablo Rosalvo on 07/02/19.
+//  Copyright © 2019 Pablo Rosalvo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
